ce675593d841130fd7dc4c93afc81c36c65e42c9
Rodrigo Alencar <rodrigovieira18@gmail.com>
2017-08-07 16:09:02 -0300

    WIP: mongo

--- test/api-skelleton.spec.js
+++ test/api-skelleton.spec.js
@@ -4,17 +4,19 @@ import assert  from 'assert'
 import request from 'supertest'
 import jwt from 'jsonwebtoken'
 import nock from 'nock'
+import mongoose from 'mongoose'
 import app  from '../lib/app'
 
 describe('API: skelleton', () => {
   process.env.SECRET_JWT = 'passphrase big very huge man'
-  let token
+  let tokenWithoutRepository
+  let tokenWithRepository
 
   afterEach( () => {
   })
 
   beforeEach( () => {
-    token = jwt
+    tokenWithoutRepository = jwt
       .sign(
       {
         github_token: 'token_github'
@@ -23,12 +25,34 @@ describe('API: skelleton', () => {
       {
         expiresIn: '1d'
       })
+
+    tokenWithRepository = jwt
+      .sign(
+      {
+        github_token: 'token_github',
+        repository: {
+           full_name: 'test_repo',
+        },
+      },
+      process.env.SECRET_JWT,
+      {
+        expiresIn: '1d'
+      })
   })
 
-  it('should return a JSON with user information', async ()=>{
+  it('should return a 401 when there is not repository information on payload token', async ()=>{
     const res = await request(app())
       .get('/api/skelleton')
-      .set('Authorization', `Bearer ${token}`)
+      .set('Authorization', `Bearer ${tokenWithoutRepository}`)
+      .send()
+
+    assert.equal(res.status, 401)
+  })
+
+  it('should return a 200 when there is repository information on payload token', async ()=>{
+    const res = await request(app())
+      .get('/api/skelleton')
+      .set('Authorization', `Bearer ${tokenWithRepository}`)
       .send()
 
     assert.equal(res.status, 200)
